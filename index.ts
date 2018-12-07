3c468f6bb4315a365c838c57a22d4384341c010f
Zekromaster <personal@zekromaster.net>
2018-12-07 20:21:22 +0100

    Licensing

--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
-/* Copyright (C) 2018 Zekromaster
-
-This file is part of MinetweakerTS.
-MinetweakerTS is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-MinetweakerTS is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with MinetweakerTS.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
+// Copyright (c) 2018 Zekromaster
+//
+// GNU GENERAL PUBLIC LICENSE
+//    Version 3, 29 June 2007
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // GENERAL LIBRARY
 /* This turns (a, b, c) into <a>.b*c*/
